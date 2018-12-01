3713119de1e1086c3a722e40f23cf8f5a1cd2c47
mkubibaev <mkubibaev@gmail.com>
2018-12-01 14:55:18 +0600

    #3 added 'send message' and 'show all messages' functions

--- js/exam-6.js
+++ js/exam-6.js
@@ -6,6 +6,9 @@ $(function () {
     var inpLastName = $('#inp-last-name');
     var btnSendProfile = $('#btn-send-profile');
     var btnEditProfile = $('#btn-edit-profile');
+    var btnSendMessage = $('#btn-send-message');
+    var inpMessage = $('#inp-message');
+    var messagesWrap = $('#messages');
 
     var baseUrl = 'http://146.185.154.90:8000/blog/mkubibaev@gmail.com/';
 
@@ -41,14 +44,45 @@ $(function () {
         inpLastName.val('');
     };
 
+    var getMessages = function () {
+        return $.ajax ({
+            method: 'GET',
+            url: baseUrl + 'posts'
+        });
+    };
+
+    var sendMessage = function (message) {
+        return $.ajax ({
+            method: 'POST',
+            url: baseUrl + 'posts',
+            data: message,
+        });
+    };
+
+    var showMessages = function (messages) {
+        messages.forEach(function (message) {
+            var card = $('<div class="card bg-light mb-2 p-3">');
+            var cardTitle = $('<span class="card-subtitle mb-2 text-muted">');
+            var cardText = $('<p class="card-text">');
+
+            cardTitle.text(message.user.firstName + ' ' + message.user.lastName + ' said:');
+            cardText.text(message.message);
+            card.append(cardTitle, cardText);
+            messagesWrap.prepend(card);
+        });
+    };
+
     var handleError = function (error) {
         console.info(error);
     };
 
-    container.hide();
-
     addProfile()
-        .then(profileModal.modal('show'))
+        .then(getProfile)
+        .then(showProfile)
+        .catch(handleError);
+
+    getMessages()
+        .then(showMessages)
         .catch(handleError);
 
     btnSendProfile.on('click', function () {
@@ -62,7 +96,6 @@ $(function () {
                 .then(getProfile)
                 .then(profileModal.modal('hide'))
                 .then(showProfile)
-                .then(container.show())
                 .then(clearProfileForm)
                 .catch(handleError);
         } else {
@@ -72,6 +105,22 @@ $(function () {
 
     btnEditProfile.on('click', function () {
         profileModal.modal('show');
-    })
+    });
+
+    btnSendMessage.on('click', function (event) {
+        event.preventDefault();
+        if (inpMessage.val()) {
+            var newMessage = {};
+
+            newMessage.message = inpMessage.val();
+
+            sendMessage(newMessage)
+                .then(inpMessage.val(''))
+                .catch(handleError);
+
+        } else {
+            alert('Enter message!');
+        }
+    });
 
 });
